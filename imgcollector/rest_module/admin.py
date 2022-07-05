be8ac6437291af2918084a97296399bf2cf2cf1b
yaroslav.koval <yaroslav@koval.page>
2022-07-05 14:47:12 +0200

    add customer type to user model

--- imgcollector/rest_module/admin.py
+++ imgcollector/rest_module/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'username', 'email', 'first_name', 'last_name',
+        'is_staff', 'customer_type'
+    )
+    list_filter = ('is_staff', 'customer_type')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- imgcollector/rest_module/models.py
+++ imgcollector/rest_module/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    CUSTOMER_TYPE = [
+        ('basic', 'Basic'),
+        ('premium', 'Premium'),
+        ('enterprise', 'Enterprise'),
+    ]
+
+    customer_type = models.CharField(default='basic', choices=CUSTOMER_TYPE, null=False, max_length=16)
