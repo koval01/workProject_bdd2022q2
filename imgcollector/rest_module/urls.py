f71d1b5769c72a781faf960c4646a9433e4f5f13
yaroslav.koval <yaroslav@koval.page>
2022-07-07 14:52:46 +0200

    update routers

--- imgcollector/rest_module/urls.py
+++ imgcollector/rest_module/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import RegisterView, PhotoLoad
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from .views import RegisterView, UserList, UserDetail, PhotoList, PhotoDetail
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -7,5 +9,10 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
-    path('load_image/', PhotoLoad.as_view(), name='list_images'),
+    path('users/', UserList.as_view(), name='users_list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user_details'),
+    path('images/', PhotoList.as_view(), name='images_list'),
+    path('images/<int:pk>/', PhotoDetail.as_view(), name='image_details'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
