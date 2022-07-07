8072f0877fd242bcf92a3c69b35eef7da5e73a5f
yaroslav.koval <yaroslav@koval.page>
2022-07-07 17:56:02 +0200

    docker update

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# switch to work dir
+cd imgcollector
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
