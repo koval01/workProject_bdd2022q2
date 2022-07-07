7a8b60cfe5fb065140d9d061af5c53afbb257db7
yaroslav.koval <yaroslav@koval.page>
2022-07-07 14:53:38 +0200

    add news views

--- imgcollector/rest_module/views.py
+++ imgcollector/rest_module/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from .models import Photo
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from .models import Photo, CustomUser as User
+from .permissions import IsObjectOwner, IsMyProfile
 
-from .serializers import RegisterSerializer, PhotoSerializer
+from .serializers import RegisterSerializer, PhotoSerializer, UserSerializer
 
 
 class RegisterView(generics.CreateAPIView):
@@ -12,7 +12,35 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
 
-class PhotoLoad(generics.CreateAPIView):
-    queryset = Photo.objects.all()
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser)
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated, IsMyProfile)
+    serializer_class = UserSerializer
+
+
+class PhotoList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        """
+        This view should return a list of all the images
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Photo.objects.filter(creator=user)
+
     permission_classes = (IsAuthenticated,)
+    serializer_class = PhotoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Photo.objects.all()
+    permission_classes = (IsAuthenticated, IsObjectOwner)
     serializer_class = PhotoSerializer
