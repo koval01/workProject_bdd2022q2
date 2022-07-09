9a708ecc8d11b5de28bc28e68963b6924b4ac0e7
yaroslav.koval <yaroslav@koval.page>
2022-07-09 16:37:58 +0200

    token and session auth

--- imgcollector/imgcollector/settings.py
+++ imgcollector/imgcollector/settings.py
@@ -84,7 +84,8 @@ REST_FRAMEWORK = {
         'rest_module.permissions.IsOwnerOrReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
