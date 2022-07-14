0f25f79ef38751d63bc407090063d5545271048b
yaroslav.koval <yaroslav@koval.page>
2022-07-14 18:51:04 +0200

    update tests

--- imgcollector/rest_module/tests/views.py
+++ imgcollector/rest_module/tests/views.py
@@ -6,3 +6,23 @@ class PollsViewsTestCase(TestCase):
     def test_users_without_auth(self):
         resp = Client().get("/users/")
         self.assertEqual(resp.status_code, 401)
+
+    def test_images_without_auth(self):
+        resp = Client().get("/images/")
+        self.assertEqual(resp.status_code, 401)
+
+    def get_api_token_auth(self):
+        resp = Client().get("/api-token-auth/")
+        self.assertEqual(resp.status_code, 405)
+
+    def post_api_token_auth(self):
+        resp_unknown_user = Client().post("/api-token-auth/")
+        self.assertEqual(resp_unknown_user.status_code, 400)
+        resp_unknown_user = Client().post("/api-token-auth/", {
+            "username": "none",
+            "password": "none"
+        })
+        self.assertEqual(resp_unknown_user.status_code, 400)
+        self.assertEqual(resp_unknown_user.json(), {
+            "non_field_errors": ["Unable to log in with provided credentials."]
+        })
