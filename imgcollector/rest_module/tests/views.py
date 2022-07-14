e29ac95dcb7ee3cce682baa26866d18d705818f6
yaroslav.koval <yaroslav@koval.page>
2022-07-14 18:45:45 +0200

    update tests

--- imgcollector/rest_module/tests/views.py
+++ imgcollector/rest_module/tests/views.py
@@ -1,8 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 
 class PollsViewsTestCase(TestCase):
 
     def test_users_without_auth(self):
-        resp = self.client.get("users/")
+        resp = Client().get("/users/")
         self.assertEqual(resp.status_code, 401)
